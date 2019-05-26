98a94d2a9d8bd2538157b9cecf262945edce29dc
Austin Pagotto <austinpagotto@gmail.com>
2019-05-25 20:07:26 -0400

    fixed new adds mongodb adds

--- Ottawa/OttawaDB.py
+++ Ottawa/OttawaDB.py
@@ -4,28 +4,46 @@ from datetime import datetime
 from pymongo import MongoClient
 from credentials import connection
 
-
+# Establish Connection
 client = MongoClient(connection)
+# Connect to Ottawa Address Database
 db = client['Address']['Ottawa']
+# Iterate over the files Address Datasets
+Address = pd.read_excel('../Ignore/ottAddress_Final.xlsx')
 
-Address = pd.read_excel('../Ignore/ottAddress0.xlsx')
-for filename in os.listdir('../Ignore'):
-    if filename[10] in ['1','2','3']:
-        tempfile = pd.read_excel('../Ignore/'+filename)
-        Address = Address.append(tempfile,sort=False)
+# for filename in os.listdir('../Ignore'):
+#     if filename[10] in ['1','2','3']:
+#         tempfile = pd.read_excel('../Ignore/'+filename)
+#         Address = Address.append(tempfile,sort=False)
 
-Address = Address.to_dict(orient='records')
+# Get Addresses already in DB
+Current = pd.DataFrame.from_records(db.find())
+# join to isolate only new Addresses
+NewAddresses = Address.merge(Current.loc[:,['PI_MUNICIPAL_ADDRESS_ID']],left_on = 'PI_MUNICIPAL_ADDRESS_ID',right_on='PI_MUNICIPAL_ADDRESS_ID',how='left',indicator=True)
+NewAddresses = NewAddresses.loc[NewAddresses['_merge']=='left_only'].drop(columns='_merge')
+# change format to be uploaded into DB
+print(NewAddresses.info())
+NewAddresses = NewAddresses.to_dict(orient='records')
+# If there are any new addresses add them
+if len(NewAddresses) > 0:
+        db.insert_many(NewAddresses)
 
-db.insert_many(Address)
-# permits = pd.read_excel('../Ignore/ottPermits.xlsx')
+# Same logic as above! but for Permits
+db = client['Permits']['Ottawa']
+Permits = pd.read_excel('../Ignore/ottPermits.xlsx')
+Permits.columns = Permits.columns.str.replace('.','')
+Current = pd.DataFrame.from_records(db.find())
+
+NewPermits = Permits.merge(Current.loc[:,['PERMIT#']],left_on = 'PERMIT#',right_on='PERMIT#',how='left',indicator=True)
+NewPermits = NewPermits.loc[NewPermits['_merge']=='left_only'].drop(columns='_merge')
+print(NewPermits.info())
+NewPermits = NewPermits.to_dict(orient='records')
+
+if len(NewPermits) > 0:
+        db.insert_many(NewPermits)
 
 
 
 
-
-
-
-# Clean up permits before geocoding
-# permits = permits.loc[permits['DESCRIPTION'].astype(str).str[:5].str.upper() == 'CONST']
 
 
--- Ottawa/ottawaAddresses.py
+++ Ottawa/ottawaAddresses.py
@@ -1,19 +1,26 @@
 import requests
 import zipfile
 import io
+import os
 import geopandas as gp
 import pandas as pd
 from shapely.geometry import Point 
 import time
 
-# read in new addres data and old address data, append and change coordinates for any new addresses
-addr = pd.read_csv('http://data.ottawa.ca/dataset/9603c5e9-7de6-49b4-b3a1-25c913badc36/resource/c069e186-0a00-4141-9339-aefa9ba8e314/download/address-points.csv',chunksize=100000)
+Address = pd.read_csv('http://data.ottawa.ca/dataset/9603c5e9-7de6-49b4-b3a1-25c913badc36/resource/c069e186-0a00-4141-9339-aefa9ba8e314/download/address-points.csv')
 
+Current = pd.read_excel('../Ignore/ottAddress0.xlsx')
+for filename in os.listdir('../Ignore'):
+    if filename[10] in ['1','2','3']:
+        tempfile = pd.read_excel('../Ignore/'+filename)
+        Current = Current.append(tempfile,sort=False)
 
-for i,chunk in enumerate(addr):
-    print(i)
-    geometry = [Point(xy) for xy in zip(chunk['POINT_X'],chunk['POINT_Y'])]
-    df = gp.GeoDataFrame(chunk,geometry=geometry)
+NewAddresses = Address.merge(Current.loc[:,['PI_MUNICIPAL_ADDRESS_ID']],left_on = 'PI_MUNICIPAL_ADDRESS_ID',right_on='PI_MUNICIPAL_ADDRESS_ID',how='left',indicator=True)
+NewAddresses = NewAddresses.loc[NewAddresses['_merge']=='left_only'].drop(columns='_merge')
+
+if len(NewAddresses) > 0:
+    geometry = [Point(xy) for xy in zip(NewAddresses['POINT_X'],NewAddresses['POINT_Y'])]
+    df = gp.GeoDataFrame(NewAddresses,geometry=geometry)
     start = time.time()
     df.crs = {'init' :'epsg:32189'}
     df = df.to_crs({'init': 'epsg:4326'})
@@ -21,7 +28,27 @@ for i,chunk in enumerate(addr):
     print(end-start)
     df['lat'] = df.geometry.y
     df['lon'] = df.geometry.x
-    df.to_excel('../Ignore/ottAddress'+str(i)+'.xlsx',index=False)
+    Current = Current.append(df,sort=False)
+    Current.to_excel('../Ignore/ottAddress_Final.xlsx',index=False)
+
+
+# INITAL Code for chunksize 
+
+# addr = pd.read_csv('http://data.ottawa.ca/dataset/9603c5e9-7de6-49b4-b3a1-25c913badc36/resource/c069e186-0a00-4141-9339-aefa9ba8e314/download/address-points.csv',chunksize=100000)
+
+
+# for i,chunk in enumerate(addr):
+#     print(i)
+#     geometry = [Point(xy) for xy in zip(chunk['POINT_X'],chunk['POINT_Y'])]
+#     df = gp.GeoDataFrame(chunk,geometry=geometry)
+#     start = time.time()
+#     df.crs = {'init' :'epsg:32189'}
+#     df = df.to_crs({'init': 'epsg:4326'})
+#     end = time.time()
+#     print(end-start)
+#     df['lat'] = df.geometry.y
+#     df['lon'] = df.geometry.x
+#     df.to_excel('../Ignore/ottAddress'+str(i)+'.xlsx',index=False)
 
 
 
